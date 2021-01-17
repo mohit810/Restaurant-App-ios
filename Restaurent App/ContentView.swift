23b3a433ec099ec4ae88bbbac475d583dc2c20a9
Gh0sT <iammohit810@gmail.com>
2021-01-17 17:40:32 +0530

    Initial Commit

---
+++ Restaurent App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurent App
+//
+//  Created by Mohit Yadav on 17/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restaurent App/Restaurent_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurent_AppApp.swift
+//  Restaurent App
+//
+//  Created by Mohit Yadav on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Restaurent_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
