4d0ef874ab9af0c0faa1c1618dff358fe78669fb
Gh0sT <iammohit810@gmail.com>
2021-01-18 23:53:01 +0530

    onBoarding Logic integration

---
+++ Restaurent App/Views/OnboardingStepView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingStepView.swift
+//  Restaurent App
+//
+//  Created by Mohit Yadav on 18/01/21.
+//
+
+import SwiftUI
+
+struct OnboardingStepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingStepView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingStepView()
+    }
+}
