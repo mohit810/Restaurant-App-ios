40826e3c9b7388ef9f58072bd6b28915ad229121
Gh0sT <iammohit810@gmail.com>
2021-01-27 23:11:02 +0530

    place holder code

--- Restaurent App/Views/Menu/MainCourseView.swift
+++ Restaurent App/Views/Menu/MainCourseView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct MainCourseView: View {
+    
+    @State var models = [RedditModel]()
+    @State var posts = [RedditPostModel]()
+    
     var body: some View {
-        Spacer()
-        Text("MainCourseView")
-        Spacer()
+        ZStack {
+            List(posts) { item in
+                DishCard(url: item.thumbnail, title: item.title, address: item.author, city: item.author, categories: ["dcxA"], kilometres: 4.33)
+                    .scaledToFit()
+            }
+        }.onAppear {
+            self.apifunc()
+        }
+    }
+    func apifunc() {
+        Api<[RedditModel]>.get(self, path: "data.children", url: "https://www.reddit.com/top.json?limit=50") { (posts) in
+            self.models = posts
+            for post in posts {
+                self.posts.append(post.data)
+            }
+            DispatchQueue.main.async() {
+                print(self.posts)
+                //        self.tableView.reloadData()
+            }
+        }
     }
 }
 
 struct MainCourseView_Previews: PreviewProvider {
     static var previews: some View {
         MainCourseView()
+    }
+}
+
+extension MainCourseView: ApiDelegate {
+    
+    func onError() {
+        DispatchQueue.main.async() {
+            let alert = UIAlertController(title: "Ups", message: "An error has occurred...", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            //self.present(alert, animated: true)
+        }
     }
 }
--- Restaurent App/Views/Menu/StartersView.swift
+++ Restaurent App/Views/Menu/StartersView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct StartersView: View {
+    
+    @State var models = [RedditModel]()
+    @State var posts = [RedditPostModel]()
+    
     var body: some View {
-        VStack {
-            Spacer()
-            Text("StartersView")
-            Spacer()
+        ZStack {
+            List(posts) { item in
+                DishCard(url: item.thumbnail, title: item.title, address: item.author, city: item.author, categories: ["dcxA"], kilometres: 4.33)
+                    .scaledToFit()
+            }
+        }.onAppear {
+            self.apifunc()
+        }
+    }
+    func apifunc() {
+        Api<[RedditModel]>.get(self, path: "data.children", url: "https://www.reddit.com/top.json?limit=50") { (posts) in
+            self.models = posts
+            for post in posts {
+                self.posts.append(post.data)
+            }
+            DispatchQueue.main.async() {
+                print(self.posts)
+                //        self.tableView.reloadData()
+            }
         }
     }
 }
@@ -20,5 +39,16 @@ struct StartersView: View {
 struct StartersView_Previews: PreviewProvider {
     static var previews: some View {
         StartersView()
+    }
+}
+
+extension StartersView: ApiDelegate {
+    
+    func onError() {
+        DispatchQueue.main.async() {
+            let alert = UIAlertController(title: "Ups", message: "An error has occurred...", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            //self.present(alert, animated: true)
+        }
     }
 }
